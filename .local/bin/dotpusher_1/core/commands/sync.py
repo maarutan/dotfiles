c8b76759563811e69dddb34cd2675cb13c64e17a
maaru <maratarzymatov288@gmail.com>
2025-06-29 02:33:48 +0600

    dotpusher

--- .local/bin/dotpusher_1/core/commands/sync.py
+++
@@ -1,9 +0,0 @@
-from ..prepare_dir import PrepareDir
-
-
-class Sync:
-    def __init__(self):
-        self.pdir = PrepareDir()
-
-    def sync(self):
-        self.pdir.walk_and_copy_on_dir("sync dotfiles ^^")
--- .local/bin/dotpusher_1/core/paths/list.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-HOME = Path.home()
-HOME_CONFIG = HOME / ".config"
-DOTPUSHER_DIR = HOME_CONFIG / "dotpusher"
-JSONC_CONFIG = DOTPUSHER_DIR / "config.jsonc"
