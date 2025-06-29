ff7cb3ed834baba43b4029b99b536fea1814cca5
maaru <maratarzymatov288@gmail.com>
2025-06-29 19:25:46 +0600

    clear repo

--- .config/zsh/node/plugins/sudo.zsh
+++
@@ -1,8 +0,0 @@
-# ┌─┐┬ ┬┌┬┐┌─┐
-# └─┐│ │ │││ │
-# └─┘└─┘─┴┘└─┘
-#--------------------------------------------
-# (c) maarutan   https://github.com/maarutan
-
-bindkey -r '^R'
-bindkey '^R' sudo-command-line
--- .zshrc
+++
@@ -1,3 +0,0 @@
-export ZSH_CONFIG="$HOME/zsh"
-source "$ZSH_CONFIG/init.zsh"
-
